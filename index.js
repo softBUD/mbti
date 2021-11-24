19ae00f091ab50d14c5ceefb35712e77b8d50b34
softBUD <chevimpala21@gmail.com>
2021-11-24 19:37:52 +0900

    1124 서버구동

---
+++ index.js
@@ -0,0 +1,25 @@
+const express= require("express");
+//모듈 로드
+const app = express();
+const path = require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+//쿼리스트링
+app.use(express.static(path.join(__dirname,"src")));
+//현재경로의 폴더의미
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "src/index.html"));
+});
+app.get("/question", (req, res) => {
+    res.sendFile(path.join(__dirname, "src/component/question.html"));
+});
+app.get("/result/[1-5]", (req, res) => {
+    res.sendFile(path.join(__dirname, "src/component/result.html"));
+});
+//"/"를 요청시에 index.html을 가져온다.
+
+app.listen(8080, ()=> {
+    console.log("Server running");
+});
